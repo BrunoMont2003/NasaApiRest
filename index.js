56d3183b39aea5c46da45d47ba90ddbf4b5630fd
brunomont <brunoadrian.mont@gmail.com>
2022-03-17 22:11:33 -0500

    filtros por rover y camara implementado

--- index.js
+++ index.js
@@ -1,34 +1,73 @@
-// import fetch from "node-fetch";
 const DEMO_KEY = "XEBPgDPAePP97QjDkGIgDWRZIcKf1rBjnDKVihXj";
 
-const marte = async () => {
-  const response = await fetch(
-    `https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&page=1&api_key=${DEMO_KEY} `
-  );
+const marte = async (rover, sol, camera) => {
+  const url = `https://api.nasa.gov/mars-photos/api/v1/rovers/${rover}/photos?sol=${sol}&page=1&${
+    camera ? "camera=" + camera : ""
+  }&api_key=${DEMO_KEY} `;
+  const response = await fetch(url);
 
+  document.getElementById("rover-name").textContent = rover;
   const { photos: fotosMarte } = await response.json();
-  console.log(fotosMarte);
-  document.getElementById("galeria").innerHTML += `
-    <div class=" col-span-full">
-      <h2 class="text-2xl text-white">Rover:
-      ${fotosMarte[0].rover.name} </h2>
-      <span class="text-slate-200">Landing date: ${fotosMarte[0].rover.landing_date} </span>
-    </div>
-  `;
-  fotosMarte.forEach((foto) => {
-    document.getElementById("galeria").innerHTML += `
-    <div class="relative border rounded shadow">
-    <img src=${foto.img_src} alt=${foto.id} class="rounded " />
-    <div class="bg-white opacity-70 p-5 absolute bottom-0 text-black font-bold flex flex-col">
-    <span>${foto.earth_date}</span>
-    <span>${foto.camera.name}</span>
-    
-    </div>
-
-    </div>
-
-    `;
-  });
+  if (fotosMarte.length > 0) {
+    document.getElementById("landing-date").textContent = await fotosMarte[0]
+      .rover.landing_date;
+    fotosMarte.forEach((foto) => {
+      document.getElementById("galeria").innerHTML += `
+      <div class="relative border rounded shadow h-auto">
+      <img src=${foto.img_src} alt=${foto.id} class="rounded" />
+      <div class="bg-white opacity-70 p-5 absolute right-0 bottom-0 text-black font-bold flex flex-col rounded">
+      <span>${foto.earth_date}</span>
+      <span>${foto.camera.name}</span>
+      
+      </div>
+  
+      </div>
+  
+      `;
+    });
+  }
 };
 
-marte();
+marte("curiosity", 0);
+var last_rover = "curiosity";
+const buscarRover = ({ target }) => {
+  document.getElementById("rover-name").textContent = target.value;
+  last_rover = target.value;
+  return target.value;
+};
+
+const buscarCamera = ({ target }) => {
+  return target.value;
+};
+
+const getSol = (rover) => {
+  switch (rover) {
+    case "curiosity":
+      return 0;
+    case "opportunity":
+      return 13;
+    case "spirit":
+      return 6;
+  }
+};
+
+document
+  .getElementById("cboRover")
+  .addEventListener("change", async (event) => {
+    document.getElementById("cboCamera").value = "";
+    const rover = buscarRover(event);
+    const sol = getSol(rover);
+    document.getElementById("galeria").innerHTML = ``;
+    await marte(rover, sol);
+  });
+
+document
+  .getElementById("cboCamera")
+  .addEventListener("change", async (event) => {
+    const sol = getSol(last_rover);
+    const camara = buscarCamera(event);
+    document.getElementById("galeria").innerHTML = ``;
+
+    await marte(last_rover, sol, camara);
+  });
+
